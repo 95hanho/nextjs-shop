dd4ca9524d2f6e3416cd081968dc904f79511e67
95hanho <hosung6674@naver.com>
2026-02-18 22:59:09 +0900

    판매자 사이트 만들기 시작

--- src/app/user/LoginClient.tsx
+++ src/app/user/LoginClient.tsx
@@ -5,39 +5,35 @@ import { SiKakaotalk, SiNaver } from "react-icons/si";
 import styles from "./LoginClient.module.scss";
 import { LoginForm } from "@/components/auth/LoginForm";
 import clsx from "clsx";
+import { FormPageShell } from "@/components/auth/FormPageShell";
 
 export default function LoginClient() {
 	return (
-		<div className={styles.loginWrap}>
-			<div className="form-wrap w-[350px] bg-white border-double border-gray-500 rounded-3xl text-center overflow-hidden shadow-sm p-5 text-lg">
-				<h2 className="py-4 text-5xl">
-					<Link href={"/"}>NextJS-SHOP</Link>
-				</h2>
-				<LoginForm />
-				<div className="flex justify-center gap-5 p-0 text-xl ">
-					<Link className="hover:underline" href={"/user/join"}>
-						회원가입
-					</Link>
-					<Link className="hover:underline" href={"/user/find/id"}>
-						아이디 찾기
-					</Link>
-					<Link className="hover:underline" href={"/user/find/password"}>
-						비밀번호 찾기
-					</Link>
-				</div>
-				<div className="my-3">
-					<button className={clsx(styles.snsButton, styles.naverLogin)}>
-						<SiNaver size={24} />
-						<span>네이버 로그인</span>
-					</button>
-				</div>
-				<div className="my-3">
-					<button className={clsx(styles.snsButton, styles.kakaoLogin)}>
-						<SiKakaotalk size={24} />
-						<span>카카오 로그인</span>
-					</button>
-				</div>
+		<FormPageShell title="로그인">
+			<LoginForm />
+			<div className="flex justify-center gap-5 p-0 text-xl ">
+				<Link className="hover:underline" href={"/user/join"}>
+					회원가입
+				</Link>
+				<Link className="hover:underline" href={"/user/find/id"}>
+					아이디 찾기
+				</Link>
+				<Link className="hover:underline" href={"/user/find/password"}>
+					비밀번호 찾기
+				</Link>
 			</div>
-		</div>
+			<div className="my-3">
+				<button className={clsx(styles.snsButton, styles.naverLogin)}>
+					<SiNaver size={24} />
+					<span>네이버 로그인</span>
+				</button>
+			</div>
+			<div className="my-3">
+				<button className={clsx(styles.snsButton, styles.kakaoLogin)}>
+					<SiKakaotalk size={24} />
+					<span>카카오 로그인</span>
+				</button>
+			</div>
+		</FormPageShell>
 	);
 }
