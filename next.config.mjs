95f2ad8cfd446eaf00bc29e32958ee2e8fcabbbe
95hanho <hosung6674@naver.com>
2026-05-17 14:59:49 +0900

    배포 이미지 경로 허용

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	// config options here
 	images: {
 		remotePatterns: [
 			{
 				protocol: "https",
 				hostname: "ehfqntuqntu.cdn1.cafe24.com",
+			},
+			{
+				protocol: "https",
+				hostname: "95hanho.pe.kr",
+				pathname: "/uploads/**",
 			},
 			{
 				protocol: "http",
